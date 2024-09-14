4d96f6ed0b57f8cef30d0fdf69177c99c0413e99
mdYeasin-cmd <yeasin200011@gmail.com>
2024-09-14 10:43:09 +0600

    Basic mock and spies

---
+++ mock_spies/utils/io.test.js
@@ -0,0 +1,15 @@
+import { it, expect, vi } from "vitest";
+import { saveTokenToFile } from "./io";
+import { promises as fs } from "fs";
+
+vi.mock("fs");
+
+it("should store the token to the file", () => {
+    const data = "dummy token";
+    const fileName = "token.txt";
+
+    saveTokenToFile(data, fileName);
+
+    // expect(saveTokenToFile(data, fileName)).resolves.toBeUndefined();
+    expect(fs.writeFile).toHaveBeenCalled();
+})
